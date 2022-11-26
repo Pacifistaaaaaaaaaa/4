5c1be6173ee25428db2592770a3403223e73bb32
unknown <xfvjykk@yandex.ru>
2022-11-26 14:11:33 +0300

    21

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿void Zadacha21()
+{
+    Console.WriteLine("Первая точка");
+    int x = Convert.ToInt32(Console.ReadLine());
+    int y = Convert.ToInt32(Console.ReadLine());
+    int z = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Вторая точка");
+    int x1 = Convert.ToInt32(Console.ReadLine());
+    int y1 = Convert.ToInt32(Console.ReadLine());
+    int z1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Расстояние = {Math.Sqrt(Math.Pow((x1-x), 2) + Math.Pow((y1-y), 2) + Math.Pow((z1-z), 2))}");
+}
+
+
+Zadacha21();
